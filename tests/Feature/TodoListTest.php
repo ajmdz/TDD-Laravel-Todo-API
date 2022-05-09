6b6594297f73f55925c7a0dab7ba1b61f106638b
ajmdotdev <contact.ajmendoza@gmail.com>
2022-05-09 15:57:44 +0800

    test: add test for fetching all todo list items

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -4,18 +4,23 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\TodoList;
 use Tests\TestCase;
 
 class TodoListTest extends TestCase
 {
-    public function test_store_todo_list()
+    use RefreshDatabase;
+
+    public function test_fetch_all_todo_list()
     {
         //prepare
+        TodoList::factory()->create(['name' => 'my list']);
 
         //perform
         $response = $this->getJson(route('todo-list.store'));
 
         //predict
         $this->assertEquals(1, count($response->json()));
+        $this->assertEquals('my list', $response->json()[0]['name']);
     }
 }
