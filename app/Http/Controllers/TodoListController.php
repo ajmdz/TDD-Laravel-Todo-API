d850e608e33effedd39be7ceeabf21a26fd3a0a3
ajmdotdev <contact.ajmendoza@gmail.com>
2022-05-10 22:49:37 +0800

    test: add test for storing new todo list item

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use App\Models\TodoList;
 
 class TodoListController extends Controller
@@ -16,5 +17,12 @@ class TodoListController extends Controller
     public function show(TodoList $list)
     {
         return response($list);
+    }
+
+    public function store(Request $request)
+    {
+        $list = TodoList::create($request->all());
+        // return response($list, Response::HTTP_CREATED);
+        return $list; // automatically returns 201 status
     }
 }
